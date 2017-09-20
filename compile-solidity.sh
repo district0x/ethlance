77110efa1c31dcac93a0d963a43dccd5e9517659
madvas <matus.lestan@gmail.com>
2017-09-20 18:26:35 +0700

    Solidity compile ignore warnings

--- compile-solidity.sh
+++ compile-solidity.sh
@@ -1,28 +1,24 @@
 #!/usr/bin/env bash
 cd resources/public/contracts/src
 
-#/Users/matus/Downloads/solidity/build/solc/solc --optimize --bin --abi ethlanceUser.sol -o ../build/
-#/Users/matus/Downloads/solidity/build/solc/solc --optimize --bin --abi ethlanceJob.sol -o ../build/
-#/Users/matus/Downloads/solidity/build/solc/solc --optimize --bin --abi ethlanceContract.sol -o ../build/
-#/Users/matus/Downloads/solidity/build/solc/solc --optimize --bin --abi ethlanceInvoice.sol -o ../build/
-#/Users/matus/Downloads/solidity/build/solc/solc --optimize --bin --abi ethlanceConfig.sol -o ../build/
-#/Users/matus/Downloads/solidity/build/solc/solc --optimize --bin --abi ethlanceViews.sol -o ../build/
-#/Users/matus/Downloads/solidity/build/solc/solc --optimize --bin --abi ethlanceSearch.sol -o ../build/
-#/Users/matus/Downloads/solidity/build/solc/solc --optimize --bin --abi ethlanceDB.sol -o ../build/
-solc --overwrite --optimize --bin --abi ethlanceUser.sol -o ../build/
-solc --overwrite --optimize --bin --abi ethlanceUser2.sol -o ../build/
-solc --overwrite --optimize --bin --abi ethlanceJob.sol -o ../build/
-solc --overwrite --optimize --bin --abi ethlanceContract.sol -o ../build/
-solc --overwrite --optimize --bin --abi ethlanceFeedback.sol -o ../build/
-solc --overwrite --optimize --bin --abi ethlanceInvoice.sol -o ../build/
-solc --overwrite --optimize --bin --abi ethlanceConfig.sol -o ../build/
-solc --overwrite --optimize --bin --abi ethlanceViews.sol -o ../build/
-solc --overwrite --optimize --bin --abi ethlanceSearchJobs.sol -o ../build/
-solc --overwrite --optimize --bin --abi ethlanceSearchFreelancers.sol -o ../build/
-solc --overwrite --optimize --bin --abi ethlanceMessage.sol -o ../build/
-solc --overwrite --optimize --bin --abi ethlanceSponsor.sol -o ../build/
-solc --overwrite --optimize --bin --abi --add-std ethlanceSponsorWallet.sol -o ../build/
-#solc --optimize --bin --abi ethlanceDB.sol -o ../build/
+function solc-err-only {
+    solc "$@" 2>&1 | grep -A 2 -i "Error"
+}
+
+solc-err-only --overwrite --optimize --bin --abi ethlanceUser.sol -o ../build/
+solc-err-only --overwrite --optimize --bin --abi ethlanceUser2.sol -o ../build/
+solc-err-only --overwrite --optimize --bin --abi ethlanceJob.sol -o ../build/
+solc-err-only --overwrite --optimize --bin --abi ethlanceContract.sol -o ../build/
+solc-err-only --overwrite --optimize --bin --abi ethlanceFeedback.sol -o ../build/
+solc-err-only --overwrite --optimize --bin --abi ethlanceInvoice.sol -o ../build/
+solc-err-only --overwrite --optimize --bin --abi ethlanceConfig.sol -o ../build/
+solc-err-only --overwrite --optimize --bin --abi ethlanceViews.sol -o ../build/
+solc-err-only --overwrite --optimize --bin --abi ethlanceSearchJobs.sol -o ../build/
+solc-err-only --overwrite --optimize --bin --abi ethlanceSearchFreelancers.sol -o ../build/
+solc-err-only --overwrite --optimize --bin --abi ethlanceMessage.sol -o ../build/
+solc-err-only --overwrite --optimize --bin --abi ethlanceSponsor.sol -o ../build/
+solc-err-only --overwrite --optimize --bin --abi --add-std ethlanceSponsorWallet.sol -o ../build/
+#solc-err-only --optimize --bin --abi ethlanceDB.sol -o ../build/
 cd ../build
 wc -c EthlanceUser.bin | awk '{print "EthlanceUser: " $1}'
 wc -c EthlanceUser2.bin | awk '{print "EthlanceUser2: " $1}'
