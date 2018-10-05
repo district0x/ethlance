a0537b5156fad8ed81b9b9d1b6cd49fb8d75421b
Benjamin Zaporzan <ben@district0x.io>
2018-10-04 22:54:09 -0400

    Started adding tests

---
+++ src/server/ethlance/server/contract/ethlance_user_factory.cljs
@@ -0,0 +1,35 @@
+(ns ethlance.server.contract.ethlance-user-factory
+  (:require
+   [cljs-web3.eth :as web3-eth]
+   [district.server.smart-contracts :as contracts]))
+
+
+(defn call [& args]
+  (apply contracts/contract-call :ethlance-user-factory args))
+
+
+(defn create-user
+  "Create a User with the given address, and with the given ipfs
+  metahash."
+  [{:keys [address metahash-ipfs]} & [opts]]
+  (call :create-user address metahash-ipfs
+        (merge {:gas 3000000} opts)))
+
+
+(defn user-by-id
+  "Get a user contract by the given user id."
+  [user-id & [opts]]
+  (call :get-user-by-id user-id
+        (merge {:gas 3000000} opts)))
+
+
+(defn user-address-by-id
+  [user-id & [opts]]
+  (call :get-user-address-by-id user-id
+        (merge {:gas 3000000} opts)))
+
+
+(defn user-count
+  [& [opts]]
+  (call :get-user-count
+        (merge {:gas 3000000} opts)))
---
+++ test/server/ethlance/server/contract/ethlance_user_factory_test.cljs
@@ -0,0 +1,11 @@
+(ns ethlance.server.contract.ethlance-user-factory-test
+  (:require
+   [clojure.test :refer [deftest is are testing]]
+   
+   [ethlance.server.contract.ethlance-user-factory
+    :as user-factory]))
+
+
+(deftest create-user
+  (testing "Creating a user and getting user count"))
+   
