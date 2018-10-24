2180539a3a912aee40f7513f7dc70669314fb10a
Benjamin Zaporzan <ben@district0x.io>
2018-10-24 11:56:54 -0400

    Changes ethlance initialization to ask for initial ethereum provider before instantiation

---
+++ src/cljs/ethlance/web3_fx.cljs
@@ -0,0 +1,31 @@
+(ns ethlance.web3-fx
+  (:require
+   [re-frame.core :refer [reg-fx dispatch]]))
+
+
+(defn supports-ethereum-provider?
+  "Determines whether the browser has the window.ethereum object. All
+  browsers are encouraged to implement this object with the method
+  `.enable` to invoke an authorization dialog as defined by EIP-1102."
+  []
+  (boolean (some-> js/window .-ethereum .-enable)))
+
+
+(defn web3-legacy? []
+  (not (some-> js/window .-ethereum)))
+
+
+(reg-fx
+ ::authorize-ethereum-provider
+ (fn [{:keys [:on-accept :on-reject :on-error :on-legacy]}]
+   #_(.log js/console "Using EIP-1102 Provider? " (supports-ethereum-provider?))
+   (cond
+     (supports-ethereum-provider?)
+     (doto (-> js/window .-ethereum .enable) ;; js/Promise
+       (.then
+        #(dispatch on-accept)
+        #(dispatch on-reject)))
+     (web3-legacy?)
+     (dispatch on-legacy)
+     :else
+     (dispatch on-error))))
