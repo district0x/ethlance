1a4d3977a5892a9bc218a9d74f4118decda966fa
Benjamin Zaporzan <ben@district0x.io>
2018-11-09 10:12:46 -0500

    MetahashStore bump

--- resources/public/contracts/src/collections/EthlanceMetahash.sol
+++ resources/public/contracts/src/collections/EthlanceMetahash.sol
@@ -14,28 +14,23 @@ library MetahashStore {
 	HashEntry[] listing;
     }
 
-    uint constant EMPLOYER_TYPE = 1;
-    uint constant CANDIDATE_TYPE = 2;
-    uint constant ARBITER_TYPE = 3;
-
-
-    function append(HashListing storage self, uint user_type, string hash_value) private {
-	self.listing.push(HashEntry(user_type, hash_value));
-    }
+    uint public constant EMPLOYER_TYPE = 1;
+    uint public constant CANDIDATE_TYPE = 2;
+    uint public constant ARBITER_TYPE = 3;
 
     
     function appendEmployer(HashListing storage self, string hash_value) internal {
-	append(self, EMPLOYER_TYPE, hash_value);
+	self.listing.push(HashEntry(EMPLOYER_TYPE, hash_value));
     }
 
     
     function appendCandidate(HashListing storage self, string hash_value) internal {
-	append(self, CANDIDATE_TYPE, hash_value);
+	self.listing.push(HashEntry(CANDIDATE_TYPE, hash_value));
     }
 
     
     function appendArbiter(HashListing storage self, string hash_value) internal {
-	append(self, ARBITER_TYPE, hash_value);
+	self.listing.push(HashEntry(ARBITER_TYPE, hash_value));
     }
     
     
