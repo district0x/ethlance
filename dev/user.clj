ef501a2770dabb76e85c24762f7e2ac9b1331e92
filip <fbielejec@gmail.com>
2019-05-09 14:47:01 +0200

    not needed

--- dev/user.clj
+++
@@ -1,7 +0,0 @@
-(ns user
-  (:require [figwheel-sidecar.repl-api :as repl-api]
-            [figwheel-sidecar.config :as config]))
- 
-(defn start-figwheel []
-  (repl-api/start-figwheel! (config/fetch-config))
-  (repl-api/cljs-repl)) 
