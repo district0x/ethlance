bb365fcbbd3ba2da8843e008a5cab6e898a089d9
Benjamin Zaporzan <ben@district0x.io>
2019-06-18 08:18:11 -0400

    started work on table

---
+++ src/ui/ethlance/ui/component/table.cljs
@@ -0,0 +1,7 @@
+(ns ethlance.ui.component.table
+  (:require
+   [reagent.core :as r]))
+
+
+(defn c-table []
+  [:div.ethlance-table "ethlance table"])
--- src/ui/ethlance/ui/page/me.cljs
+++ src/ui/ethlance/ui/page/me.cljs
@@ -13,25 +13,30 @@
    [ethlance.ui.component.search-input :refer [c-chip-search-input]]
    [ethlance.ui.component.currency-input :refer [c-currency-input]]
    [ethlance.ui.component.inline-svg :refer [c-inline-svg]]
-   [ethlance.ui.component.tabular-layout :refer [c-tabular-layout]]))
-
+   [ethlance.ui.component.tabular-layout :refer [c-tabular-layout]]
+   [ethlance.ui.component.table :refer [c-table]]))
 
 (defn c-me-sidebar []
   [:div.sidebar
    [:span.label "Employer"]
-   [:a "My Jobs"]
-   [:a "My Contracts"]
-   [:a "My Invoices"]
+   [:a {:href "#/me/jobs"} "My Jobs"]
+   [:a {:href "#/me/candidates"} "My Contracts"]
+   [:a {:href "#"} "My Invoices"]
    [:hr]
    [:span.label "Candidate"]
-   [:a "My Jobs"]
-   [:a "My Contracts"]
-   [:a "My Invoices"]
+   [:a {:href "#"} "My Jobs"]
+   [:a {:href "#"} "My Contracts"]
+   [:a {:href "#"} "My Invoices"]
    [:hr]
    [:span.label "Arbiter"]
-   [:a "My Jobs"]
-   [:a "My Contracts"]
-   [:a "My Invoices"]])
+   [:a {:href "#"} "My Jobs"]
+   [:a {:href "#"} "My Contracts"]
+   [:a {:href "#"} "My Invoices"]])
+
+
+(defn c-invitations-tab []
+  [:div.invitations-tab
+   [c-table]])
 
 
 (defmethod page :route.me/index []
@@ -43,8 +48,7 @@
         {:default-tab 0}
         
         {:label "Invitations"}
-        [:div.invitations-tab
-         "Invitations"]
+        [c-invitations-tab]
 
         {:label "Pending Proposals"}
         [:div.pending-proposals-tab
