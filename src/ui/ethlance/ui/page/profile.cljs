f93e0442a25f5450029fd3ccb6de9a4d3a3fd268
Benjamin Zaporzan <ben@district0x.io>
2019-08-21 18:46:41 -0400

    Completed first section of profile page

--- src/ui/ethlance/ui/page/profile.cljs
+++ src/ui/ethlance/ui/page/profile.cljs
@@ -6,6 +6,7 @@
    [ethlance.shared.enumeration.currency-type :as enum.currency]
 
    ;; Ethlance Components
+   [ethlance.ui.component.button :refer [c-button c-button-icon-label]]
    [ethlance.ui.component.circle-button :refer [c-circle-icon-button]]
    [ethlance.ui.component.currency-input :refer [c-currency-input]]
    [ethlance.ui.component.inline-svg :refer [c-inline-svg]]
@@ -15,11 +16,83 @@
    [ethlance.ui.component.search-input :refer [c-chip-search-input]]
    [ethlance.ui.component.table :refer [c-table]]
    [ethlance.ui.component.tabular-layout :refer [c-tabular-layout]]
-   [ethlance.ui.component.tag :refer [c-tag c-tag-label]]))
+   [ethlance.ui.component.tag :refer [c-tag c-tag-label]]
+   [ethlance.ui.component.profile-image :refer [c-profile-image]]))
+
+
+(defn c-candidate-profile []
+  [:<>
+   [:div.candidate-profile
+    [:div.title
+     [:div.profile-image
+      [c-profile-image {}]]
+     [:div.name "Clement Lesaenge"]
+     [:div.detail "Graphic Design | Joined 8 Days Ago"]]
+    [:div.biography
+     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+     eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+     enim ad minim veniam, quis nostrud exercitation ullamco laboris
+     nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+     reprehenderit in voluptate velit esse cillum dolore eu fugiat
+     nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+     sunt in culpa qui officia deserunt mollit anim id est
+     laborum."]
+    [:div.rating
+     [c-rating {:rating 3 :color :primary}]
+     [:span "(8)"]]
+    [:div.location "United States, New York"]
+    [:div.detail-listing
+     [:div.languages
+      [:span "Languages"]
+      [c-tag {} [c-tag-label "English"]]
+      [c-tag {} [c-tag-label "German"]]]
+     [:div.skills
+      [:span "Skills"]
+      [c-tag {} [c-tag-label "Javascript Programming"]]
+      [c-tag {} [c-tag-label "HTML / CSS"]]]]
+      
+    [:div.button-listing
+     [c-button
+      {:size :normal}
+      [c-button-icon-label {:icon-name :github :label-text "Github"}]]
+     [c-button
+      {:size :normal}
+      [c-button-icon-label {:icon-name :linkedin :label-text "LinkedIn"}]]]]
+
+   [:div.job-listing
+    [:div.title "Created Jobs"]]
+
+   [:div.feedback-listing
+    [:div.title "Feedback"]
+    [:div.sub-title "Smart Contract Hacker"]]])
+  
+
+
+(defn c-employer-profile []
+  [:<>
+   [:div.employer-profile
+    "Employer Profile"]])
+
+
+(defn c-arbiter-profile []
+  [:<>
+   [:div.arbiter-profile
+    "Arbiter Profile"]])
 
 
 (defmethod page :route.user/profile []
   (let []
     (fn []
       [c-main-layout {:container-opts {:class :profile-main-container}}
-       "Profile"])))
+       [c-tabular-layout
+        {:key "profile-tabular-layout"
+         :default-tab 0}
+
+        {:label "Candidate Profile"}
+        [c-candidate-profile]
+
+        {:label "Employer Profile"}
+        [c-employer-profile]
+
+        {:label "Arbiter Profile"}
+        [c-arbiter-profile]]])))
