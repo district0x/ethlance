e4af9a6a7552bb335981190cc7dce84d226c7f18
Benjamin Zaporzan <benzaporzan@gmail.com>
2019-09-10 23:28:48 -0400

    Finished candidates page (mirror of arbiter's page)

--- src/ui/ethlance/ui/page/candidates.cljs
+++ src/ui/ethlance/ui/page/candidates.cljs
@@ -1,9 +1,11 @@
 (ns ethlance.ui.page.candidates
+  "General Candidate Listings on ethlance"
   (:require
    [taoensso.timbre :as log]
    [district.ui.component.page :refer [page]]
 
    [ethlance.shared.enumeration.currency-type :as enum.currency]
+   [ethlance.shared.constants :as constants]
 
    ;; Ethlance Components
    [ethlance.ui.component.main-layout :refer [c-main-layout]]
@@ -12,15 +14,102 @@
    [ethlance.ui.component.radio-select :refer [c-radio-select c-radio-search-filter-element]]
    [ethlance.ui.component.search-input :refer [c-chip-search-input]]
    [ethlance.ui.component.currency-input :refer [c-currency-input]]
-   [ethlance.ui.component.inline-svg :refer [c-inline-svg]]))
+   [ethlance.ui.component.inline-svg :refer [c-inline-svg]]
+   [ethlance.ui.component.select-input :refer [c-select-input]]
+   [ethlance.ui.component.mobile-search-filter :refer [c-mobile-search-filter]]
+   [ethlance.ui.component.profile-image :refer [c-profile-image]]))
 
 
 (defn c-candidate-search-filter []
-  [:div.search-filter "search-filter"])
+  [:div.search-filter
+   [:div.category-selector
+    [c-select-input
+     {:label "All Categories"
+      :default-selection "All Categories"
+      :color :secondary
+      :selections ["All Categories" "Software Development" "Web Design"]}]]
+   [:span.rating-label "Min. Rating"]
+   [c-rating {:rating 1 :color :white :size :small
+              :on-change (fn [index] (log/debug "Min. Rating: " index))}]
+
+   [:span.rating-label "Max. Rating"]
+   [c-rating {:rating 5 :color :white :size :small
+              :on-change (fn [index] (log/debug "Max. Rating: " index))}]
+
+   [:span.selection-label "Payment Type"]
+   [c-radio-select 
+    {:on-selection (fn [selection] (log/debug (str "Payment Selection: " selection)))
+     :default-selection :fixed}
+    [:fixed [c-radio-search-filter-element "Fixed Price"]]
+    [:percentage [c-radio-search-filter-element "Percentage of Dispute"]]]
+
+   [:div.country-selector
+    [c-select-input
+     {:label "Select Country"
+      :selections constants/countries
+      :search-bar? true
+      :color :secondary
+      :default-search-text "Search Countries"}]]])
+
+
+(defn c-candidate-mobile-search-filter
+  []
+  [c-mobile-search-filter
+   [:div.category-selector
+    [c-select-input
+     {:label "All Categories"
+      :default-selection "All Categories"
+      :color :secondary
+      :selections ["All Categories" "Software Development" "Web Design"]}]]
+   [:span.rating-label "Min. Rating"]
+   [c-rating {:rating 1 :color :white :size :small
+              :on-change (fn [index] (log/debug "Min. Rating: " index))}]
+
+   [:span.rating-label "Max. Rating"]
+   [c-rating {:rating 5 :color :white :size :small
+              :on-change (fn [index] (log/debug "Max. Rating: " index))}]
+
+   [:span.selection-label "Payment Type"]
+   [c-radio-select 
+    {:on-selection (fn [selection] (log/debug (str "Payment Selection: " selection)))
+     :default-selection :fixed}
+    [:fixed [c-radio-search-filter-element "Fixed Price"]]
+    [:percentage [c-radio-search-filter-element "Percentage of Dispute"]]]
+
+   [:div.country-selector
+    [c-select-input
+     {:label "Select Country"
+      :selections constants/countries
+      :search-bar? true
+      :color :secondary
+      :default-search-text "Search Countries"}]]])
+
+
+(defn c-candidate-element
+  [candidate]
+  [:div.candidate-element
+   [:div.profile
+    [c-profile-image {}]
+    [:div.name "Brian Curran"]
+    [:div.title "Content Creator, Web Developer, Blockchain Analyst"]]
+   [:div.price "$15 / Fixed Price"]
+   [:div.tags
+    [c-tag {} [c-tag-label "System Administration"]]
+    [c-tag {} [c-tag-label "Game Design"]]
+    [c-tag {} [c-tag-label "C++ Programming"]]
+    [c-tag {} [c-tag-label "HopScotch Master"]]]
+   [:div.rating
+    [c-rating {:rating 3}]
+    [:div.label "(4)"]]
+   [:div.location "New York, United States"]])
 
 
 (defn c-candidate-listing []
-  [:div.listing "listing"])
+  [:<>
+   (doall
+    (for [candidate (range 10)]
+      ^{:key (str "candidate-" candidate)}
+      [c-candidate-element candidate]))])
 
 
 (defmethod page :route.user/candidates []
@@ -28,6 +117,7 @@
     (fn []
       [c-main-layout {:container-opts {:class :candidates-main-container}}
        [c-candidate-search-filter]
-       [:div.listing {:key "listing"}
+       [c-candidate-mobile-search-filter]
+       [:div.candidate-listing.listing {:key "listing"}
         [c-chip-search-input {:default-chip-listing #{"C++" "Python"}}]
         [c-candidate-listing]]])))
