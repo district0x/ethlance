9200de2a2c8423de61805cb8dca7bb7920043693
Benjamin Zaporzan <benzaporzan@gmail.com>
2019-09-30 16:46:34 -0400

    First batch of documentation

--- src/ui/ethlance/ui/component/checkbox.cljs
+++ src/ui/ethlance/ui/component/checkbox.cljs
@@ -5,6 +5,28 @@
 
 
 (defn c-labeled-checkbox
+  "Checkbox Input Component
+
+  # Keyword Arguments
+
+  opts - Optional Arguments
+
+  # Optional Arguments (opts)
+
+  :label - The label to display after the checkbox.
+
+  :on-change - Accepts a function with one argument, which is called
+  when the value of the checkbox changes between `true` and `false`.
+
+  :default-checked? - Determines whether initially the checkbox is
+  checked or not. [default: false]
+
+  # Examples
+
+  ```clojure
+  [c-labeled-checkbox {:label \"I Agree to the Terms and Services.\"}]
+  ```
+  "
   [{:keys [label on-change default-checked?] :as opts}]
   (let [opts (dissoc opts :label :on-change :default-checked?)
         *checked? (r/atom default-checked?)]
