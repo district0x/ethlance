0b136ae8ce32b301203e509cc0572a5247d4a400
Benjamin Zaporzan <benzaporzan@gmail.com>
2019-10-15 15:26:47 -0400

    Burning down the house

--- test/server/ethlance/server/test_runner.cljs
+++ test/server/ethlance/server/test_runner.cljs
@@ -12,27 +12,9 @@
 
    ;; Contract Tests
    [ethlance.server.contract.token-test]
-   [ethlance.server.contract.ethlance-user-factory-test]
-   [ethlance.server.contract.ethlance-user-test]
-   [ethlance.server.contract.ethlance-job-factory-test]
-   [ethlance.server.contract.ethlance-job-store-test]
-   [ethlance.server.contract.ethlance-work-contract-test]
-   [ethlance.server.contract.ethlance-invoice-test]
-   [ethlance.server.contract.ethlance-dispute-test]
-   [ethlance.server.contract.ethlance-comment-test]
-   [ethlance.server.contract.ethlance-feedback-test]
-   [ethlance.server.contract.ethlance-token-store-test]
-
    
    ;; Database Tests
-   [ethlance.server.db-test]
-   [ethlance.server.model.user-test]
-   [ethlance.server.model.candidate-test]
-   [ethlance.server.model.arbiter-test]
-   [ethlance.server.model.employer-test]
-   [ethlance.server.model.job-test]
-   [ethlance.server.model.comment-test]
-   [ethlance.server.model.feedback-test]
+   #_[ethlance.server.db-test]
    
    ;; Misc Tests
    [ethlance.server.core-test]
@@ -62,27 +44,10 @@
    'ethlance.shared.enumeration.contract-status-test
 
    ;; Database Tests
-   'ethlance.server.db-test
-   'ethlance.server.model.user-test
-   'ethlance.server.model.candidate-test
-   'ethlance.server.model.arbiter-test
-   'ethlance.server.model.employer-test
-   'ethlance.server.model.job-test
-   'ethlance.server.model.comment-test
-   'ethlance.server.model.feedback-test
+   #_'ethlance.server.db-test
 
    ;; Smart Contract Tests
-   'ethlance.server.contract.token-test
-   'ethlance.server.contract.ethlance-user-factory-test
-   'ethlance.server.contract.ethlance-user-test
-   'ethlance.server.contract.ethlance-job-factory-test
-   'ethlance.server.contract.ethlance-job-store-test
-   'ethlance.server.contract.ethlance-work-contract-test
-   'ethlance.server.contract.ethlance-invoice-test
-   'ethlance.server.contract.ethlance-dispute-test
-   'ethlance.server.contract.ethlance-comment-test
-   'ethlance.server.contract.ethlance-feedback-test
-   'ethlance.server.contract.ethlance-token-store-test))
+   'ethlance.server.contract.token-test))
 
 
 
