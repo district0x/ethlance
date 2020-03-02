94cd294b3de4247ac0cc7d2f328533b03b8e0633
Benjamin Zaporzan <ben@district0x.io>
2020-03-02 15:38:19 +0000

    Finished up draft of class diagram

--- docs/bin/generate-html.el
+++ docs/bin/generate-html.el
@@ -29,34 +29,6 @@ Generate HTML File from Org File
     (print-help-message)
     (done)))
 
-;; Configure Emacs to handle Image Exports correctly
-
-(defun org-custom-link-img-follow (path)
-  (org-open-file-with-emacs
-   (format "%s" path)))
-
-
-(defun org-custom-link-img-export (path desc format)
-  "Files pulled from the /docs/images folder have their relative
-links fixed for the public website."
-  (let ((directory (file-name-directory path))
-        (filename (file-name-nondirectory path)))
-    (cond
-     
-     ;; Fix relative links to ../images folder
-     ((and (eq format 'html)
-           (string= directory "../images/"))
-      (format "<img src=\"/images/%s\" alt=\"%s\"/>" filename desc))
-
-     ;; Default html export for paths outside of ../images folder
-     ((eq format 'html)
-      (format "<img src=\"%s\" alt=\"%s\"/>" path desc)))))
-
-(org-link-set-parameters
- "img"
- :follow 'org-custom-link-img-follow
- :export 'org-custom-link-img-export)
-
 
 ;;
 ;; BEGIN EXPORT
