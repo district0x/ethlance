53d345cba667c5b739a5551da187fe709dfa86d9
Benjamin Zaporzan <ben@district0x.io>
2020-03-04 01:01:41 +0000

    Doc Generation Finished with DB Dia Diagram Draft

--- docs/bin/publish-ipfs.sh
+++ docs/bin/publish-ipfs.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+CWD=`pwd`
+IPFS_HASH_FILE=.site-hash
 IPFS_GATEWAY="https://ipfs.io"
-echo "Site: $1"
+echo "Site: $CWD/$1"
+echo "File: $CWD/$IPFS_HASH_FILE"
 HASH=`ipfs add -qr $1 | tail -n 1 | cut -d" " -f1`
+URL="$IPFS_GATEWAY/ipfs/$HASH"
 echo "HASH: $HASH"
-echo "URL:  $IPFS_GATEWAY/ipfs/$HASH/index.html"
-
+echo "URL:  $URL"
+echo "$HASH" > $IPFS_HASH_FILE
