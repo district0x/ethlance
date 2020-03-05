de883537a53419577419987161f295e0ac9b091a
Benjamin Zaporzan <ben@district0x.io>
2020-03-05 18:09:07 +0000

    Refined database diagram (2nd draft)

--- docs/bin/publish-ipfs.sh
+++ docs/bin/publish-ipfs.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
+# Adds the given directory to IPFS, and returns the HASH and URL.
+# publish-ipfs.sh <directory>
+#
+# Also creates two files:
+# - $IPFS_HASH_FILE: Contains the ipfs hash of the given directory
+# - $IPFS_URL_FILE:  Contains the URL of the given directory
 
 CWD=`pwd`
+
 IPFS_HASH_FILE=.site-hash
+IPFS_URL_FILE=.site-url
+
 IPFS_GATEWAY="https://ipfs.io"
+
 echo "Site: $CWD/$1"
 echo "File: $CWD/$IPFS_HASH_FILE"
+
 HASH=`ipfs add -qr $1 | tail -n 1 | cut -d" " -f1`
 URL="$IPFS_GATEWAY/ipfs/$HASH"
+
 echo "HASH: $HASH"
-echo "URL:  $URL"
 echo "$HASH" > $IPFS_HASH_FILE
+
+echo "URL:  $URL"
+echo "$URL" > $IPFS_URL_FILE
