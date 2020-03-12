34393e3f9ad144723ca123a5a390bf4502b818c5
Juan Monetta <jpmonettas@gmail.com>
2020-03-12 15:38:58 -0300

    Add EthlanceJobs and EthlanceIssuer

--- test/ethlance/server/test_runner.cljs
+++ test/ethlance/server/test_runner.cljs
@@ -1,8 +1,13 @@
 (ns ethlance.server.test-runner
   (:require [cljs.nodejs :as nodejs]
             [doo.runner :refer-macros [doo-tests]]
-            [ethlance.server.graphql.resolvers-test]))
+            [ethlance.server.graphql.resolvers-test]
+            [ethlance.server.contract.bounty-test]
+            [ethlance.server.contract.ethlance-jobs-test]))
 
 (nodejs/enable-util-print!)
 
-(doo-tests 'ethlance.server.graphql.resolvers-test)
+(doo-tests #_'ethlance.server.graphql.resolvers-test
+           'ethlance.server.contract.bounty-test
+           'ethlance.server.contract.ethlance-jobs-test
+           )
