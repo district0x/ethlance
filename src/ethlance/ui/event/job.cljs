4a559cf821baf7dfc21e27404d95ea935d9544eb
Benjamin Zaporzan <ben@district0x.io>
2020-04-10 00:04:10 +0000

    More work on re-frame job listings and mock data

--- src/ethlance/ui/event/job.cljs
+++ src/ethlance/ui/event/job.cljs
@@ -3,14 +3,19 @@
   (:require
    [re-frame.core :as re]))
 
-(def default-job
-  #:ethlance.job-listing{:skills #{}
-                         :min-rating 1
-                         :max-rating 5
-                         :min-hourly-rate nil
-                         :max-hourly-rate nil
-                         :min-num-feedbacks nil
-                         :payment-type :hourly-rate
-                         :experience-level :novice
-                         :country nil})
-
+(def state-key :ethlance.job)
+(def state-default
+  {:title nil
+   :employer nil
+   :arbiter nil
+   :skills #{}
+   :min-rating 1
+   :max-rating 5
+   :min-hourly-rate nil
+   :max-hourly-rate nil
+   :min-num-feedbacks nil
+   :payment-type :hourly-rate
+   :project-length nil
+   :availability :full-time
+   :experience-level :novice
+   :country nil})
--- src/ethlance/ui/events.cljs
+++ src/ethlance/ui/events.cljs
@@ -3,14 +3,30 @@
   (:require 
    [re-frame.core :as re]
 
+   ;; District UI Events
+   [district.ui.logging.events]
+
    ;; Ethlance Component Event Handlers
    [ethlance.ui.component.modal.events]
 
    ;; Ethlance Main Event Handlers
-   [ethlance.ui.event.sign-in]))
+   [ethlance.ui.event.sign-in]
+   [ethlance.ui.event.job]
+   [ethlance.ui.event.job-listing]))
 
 
 (defn initialize
+  "Sets initial db state."
   [{:keys [db] :as cofx} _]
-  {:db (-> db)})
+  {:db (assoc db
+              ethlance.ui.event.job/state-key
+              ethlance.ui.event.job/state-default
+              ethlance.ui.event.job-listing/state-key
+              ethlance.ui.event.job-listing/state-default)
+   :log/info ["re-frame Initialized app state"]})
 
+;;
+;; Registered Events
+;;
+
+(re/reg-event-fx :ethlance/initialize initialize)
