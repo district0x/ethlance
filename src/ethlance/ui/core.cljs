7103c8adbc374ee8d6ac00e0685cb21e7eaff308
filip <fbielejec@gmail.com>
2020-09-28 14:52:26 +0200

    add missing mount module

--- src/ethlance/ui/core.cljs
+++ src/ethlance/ui/core.cljs
@@ -1,30 +1,28 @@
 (ns ethlance.ui.core
   (:require
+   district.ui.component.router
+   district.ui.ipfs
+   district.ui.logging
+   district.ui.reagent-render
+   district.ui.router
+   district.ui.web3-accounts
+   [ethlance.ui.config :as ui.config]
+   ethlance.ui.effects
+   ethlance.ui.events
+   ethlance.ui.pages
+   ethlance.ui.subscriptions
+   [ethlance.ui.util.injection :as util.injection]
    [mount.core :as mount :refer [defstate]]
    [re-frame.core :as re]
+   [re-frisk.core :as re-frisk]
    [taoensso.timbre :as log]
-
-   ;; District UI Components
-   [district.ui.component.router]
-   [district.ui.ipfs]
-   [district.ui.logging]
-   [district.ui.reagent-render]
-   [district.ui.router]
-
-   ;; Ethlance
-   [ethlance.ui.config :as ui.config]
-   [ethlance.ui.effects]
-   [ethlance.ui.events]
-   [ethlance.ui.pages]
-   [ethlance.ui.subscriptions]
-   [ethlance.ui.util.injection :as util.injection]))
-
+   ))
 
 (enable-console-print!)
 
-
 (defn ^:export init []
   (let [main-config (ui.config/get-config)]
+    (re-frisk/enable-re-frisk!)
     (.log js/console (clj->js main-config))
     (util.injection/inject-data-scroll! {:injection-selector "#app"})
 
@@ -36,6 +34,3 @@
     (re/dispatch-sync [:ethlance/initialize main-config])
 
     ::started))
-
-
-(defonce started? (init))
