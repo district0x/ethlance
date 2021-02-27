9a606fea6b8135bc3a715c17d420c7d981a4ae93
Madis Nõmme <me@mad.is>
2021-02-27 08:48:47 +0000

    Keep jobs data separately in app-db

--- src/ethlance/ui/page/profile/subscriptions.cljs
+++ src/ethlance/ui/page/profile/subscriptions.cljs
@@ -6,29 +6,23 @@
 (def create-get-handler #(subscription.utils/create-get-handler profile.events/state-key %))
 
 (re/reg-sub
-  ::job-roles
+  ::job-roles-raw
   (fn [db _] (:job-role-search db)))
 
-(defn- prepare-role-data [job-role]
-  (select-keys job-role [:job-id :start-date :title :status]))
+(re/reg-sub
+  ::job-roles-per-address-role
+  :<- [::job-roles-raw]
+  (fn [job-roles [_ address role]]
+    (get-in job-roles [address role])))
 
 (re/reg-sub
-  ::candidate-jobs
-  :<- [::job-roles]
-  (fn [job-roles [_ candidate-id]]
-    (let [roles (get-in job-roles [candidate-id "CANDIDATE"])]
-      (map prepare-role-data roles))))
+  ::jobs
+  (fn [db _] (:jobs db)))
 
 (re/reg-sub
-  ::employer-jobs
-  :<- [::job-roles]
-  (fn [job-roles [_ employer-id]]
-    (let [roles (get-in job-roles [employer-id "EMPLOYER"])]
-          (map prepare-role-data roles))))
+  ::job-roles
+  (fn [[_ address role] _]
+    [(re/subscribe [::jobs]) (re/subscribe [::job-roles-per-address-role address role])])
 
-(re/reg-sub
-  ::arbiter-jobs
-  :<- [::job-roles]
-  (fn [job-roles [_ arbiter-id]]
-    (let [roles (get-in job-roles [arbiter-id "ARBITER"])]
-          (map prepare-role-data roles))))
+  (fn [[jobs role-data] _]
+    (map #(assoc % :title (get-in jobs [(:job-id %) :job/title])) role-data)))
