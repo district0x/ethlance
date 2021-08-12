d90d3ded30ec93a9c2f0f76a39f026ff70357dc8
Madis Nõmme <me@mad.is>
2021-08-12 08:33:28 +0000

    Change config to simplify development

--- truffle.js
+++ truffle.js
@@ -23,25 +23,43 @@ let parameters = {
 parameters.dev = parameters.qa;
 
 
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const mnemonic = "easy leave proof verb wait patient fringe laptop intact opera slab shine";
+
 module.exports = {
   env: ETHLANCE_ENV,
-  smart_contracts_path: __dirname + smartContractsPaths [ETHLANCE_ENV],
+  smart_contracts_path: __dirname + smartContractsPaths[ETHLANCE_ENV],
+  contracts_directory: __dirname + "/contracts/zksync",
   contracts_build_directory: __dirname + '/resources/public/contracts/build/',
   parameters : parameters [ETHLANCE_ENV],
 
   networks: {
+    development: {
+      port: 9545,
+      host: "0.0.0.0",
+      gas: 2 * 70000,
+      network_id: "*"
+    },
+
     ganache: {
-      host: 'localhost',
+      host: 'd0x-vm',
       port: 8549,
       gas: 6e6, // gas limit
       gasPrice: 20e9, // 20 gwei, default for ganache
       network_id: '*'
     },
+
+    hostia: { // Truffle Ganache GUI app running on host machine
+      host: "192.168.12.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "5777",       // Any network (default: none)
+      provider: () => new HDWalletProvider(mnemonic, `http://192.168.12.1:7545`),
+    },
   },
 
   compilers: {
     solc: {
-      version: "0.5.8",
+      version: "0.8.4",
     }
   }
 }
