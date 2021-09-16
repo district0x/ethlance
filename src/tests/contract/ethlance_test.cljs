ab72cffc11be38f6195e3eb7c86eede41fc866a3
Madis Nõmme <me@mad.is>
2021-09-16 10:28:19 +0000

    Implement Job#cancelInvoice w/ restrictions

--- src/tests/contract/ethlance_test.cljs
+++ src/tests/contract/ethlance_test.cljs
@@ -222,21 +222,19 @@
   [job-data token]
   (first
     (filter #(= (get-in % [:token :tokenContract :tokenType])
-                (contract-constants/token-type :erc1155) )
+                (contract-constants/token-type token) )
             (get-in job-data [:offered-values]))))
 
-(deftest invoice-flows
+#_ (deftest invoice-flows
   (testing "invoice flow (create/pay/cancel)"
     (async done
            (go
              (let [[_owner employer worker] (<! (web3-eth/accounts @web3))
                    job-data (<! (create-initialized-job
-                                  [
-                                   ; (partial fund-in-eth 0.02)
-                                   ; (partial fund-in-erc20 employer 2)
-                                   ; (partial fund-in-erc721 employer)
-                                   (partial fund-in-erc1155 employer 5)
-                                   ]))
+                                  [(partial fund-in-eth 0.02)
+                                   (partial fund-in-erc20 employer 2)
+                                   (partial fund-in-erc721 employer)
+                                   (partial fund-in-erc1155 employer 5)]))
                    funding-tx-receipt (:tx-receipt job-data)
                    job-address (:job job-data)
                    invoice-amount-eth 0.01
@@ -251,8 +249,7 @@
                    invoice-event-2 (<! (smart-contracts/contract-event-in-tx :ethlance :InvoiceCreated tx-receipt-2))
 
                    invoice-1-id (int (:invoice-id invoice-event))
-                   invoice-2-id (int (:invoice-id invoice-event-2))
-                   ]
+                   invoice-2-id (int (:invoice-id invoice-event-2))]
 
                  (is (not (nil? tx-receipt)))
                  (is (< invoice-1-id invoice-2-id) "Different invoices must have different ids (and they're auto-increment)")
@@ -261,7 +258,7 @@
                  (is (= nil (<! (smart-contracts/contract-send [:job job-address] :pay-invoice [invoice-1-id "0x0"] {:from worker}))), "Only job creator(employer) can pay invoices")
 
                  ; Pay ETH invoice
-                 #_ (let [worker-eth-balance-before (<? (web3-eth/get-balance @web3 worker))
+                 (let [worker-eth-balance-before (<? (web3-eth/get-balance @web3 worker))
                        tx-pay-invoice (<! (smart-contracts/contract-send [:job job-address] :pay-invoice [invoice-1-id "0x0"] {:from employer}))
                        event-pay-invoice (<! (smart-contracts/contract-event-in-tx :ethlance :InvoicePaid tx-pay-invoice))
                        worker-eth-balance-after (<? (web3-eth/get-balance @web3 worker))
@@ -271,7 +268,7 @@
                    (is (= worker-eth-change invoice-amount-eth)))
 
                  ; Pay ERC20 invoice
-                 #_ (let [erc-20-token-amount 1
+                 (let [erc-20-token-amount 1
                        worker-initial-erc20-balance (<? (smart-contracts/contract-call :token :balance-of [worker]))
                        [erc-20-invoice-amounts _extra] (<? (fund-in-erc20 employer erc-20-token-amount [] {}))
                        ; Create invoices
@@ -285,7 +282,7 @@
                    (is (= worker-erc20-balance-change erc-20-token-amount)))
 
                  ; Pay ERC721 (NFT) invoice
-                 #_ (let [nft-offer (offer-from-job-data job-data :erc721)
+                 (let [nft-offer (offer-from-job-data job-data :erc721)
                        token-id (get-in nft-offer [:token :tokenId])
                        tx-receipt (<! (smart-contracts/contract-send [:job job-address] :create-invoice [[nft-offer] "0x0"] {:from worker}))
                        invoice-event (<! (smart-contracts/contract-event-in-tx :ethlance :InvoiceCreated tx-receipt))
@@ -306,7 +303,7 @@
                    (is (= (int final-worker-balance) offered-token-amount) "In the end the offered NFT1155 must end up at worker's account"))
                (done))))))
 
-(deftest adding-candidate
+#_ (deftest adding-candidate
   (testing "Job#addCandidate"
     (async done
            (go
@@ -336,4 +333,44 @@
               ; Try adding candidate by user other than job creator (should fail)
               (let [tx-receipt (<! (smart-contracts/contract-send [:job job-address] :add-candidate [worker empty-ipfs-data] {:from worker}))]
                 (is (= nil tx-receipt))))
+             (done)))))
+
+(deftest cancel-invoice-test
+  (testing "Job#cancelInvoice"
+    (async done
+           (go
+             (let [[_owner employer worker] (<! (web3-eth/accounts @web3))
+                   job-data (<! (create-initialized-job [(partial fund-in-erc20 employer 2)]))
+                   token-offer (offer-from-job-data job-data :erc20)
+                   job-address (:job job-data)
+                   empty-ipfs-data "0x0"
+                   invoice-tx-receipt (<! (smart-contracts/contract-send [:job job-address] :create-invoice [[token-offer] "0x0"] {:from worker}))
+                   invoice-event (<! (smart-contracts/contract-event-in-tx :ethlance :InvoiceCreated invoice-tx-receipt))
+                   invoice-id (int (:invoice-id invoice-event))]
+
+               ; Successful cancel
+               (let [tx-cancel-invoice (<! (smart-contracts/contract-send [:job job-address] :cancel-invoice [invoice-id "0x0"] {:from worker}))
+                     cancel-event (<! (smart-contracts/contract-event-in-tx :ethlance :InvoiceCancelled tx-cancel-invoice))]
+                 (is (= (int (:invoice-id cancel-event)) invoice-id)))
+
+               ; Cancel same invoice again (tx fails)
+               (let [tx-cancel-invoice (<! (smart-contracts/contract-send [:job job-address] :cancel-invoice [invoice-id "0x0"] {:from worker}))]
+                 (is (= nil tx-cancel-invoice)))
+
+               ; Create new invoice, pay it & try to cancel (should fail)
+               (let [invoice-tx-receipt (<! (smart-contracts/contract-send [:job job-address] :create-invoice [[token-offer] "0x0"] {:from worker}))
+                     invoice-event (<! (smart-contracts/contract-event-in-tx :ethlance :InvoiceCreated invoice-tx-receipt))
+                     invoice-id (int (:invoice-id invoice-event))
+
+                     tx-pay-invoice (<! (smart-contracts/contract-send [:job job-address] :pay-invoice [invoice-id "0x0"] {:from employer}))
+                     event-pay-invoice (<! (smart-contracts/contract-event-in-tx :ethlance :InvoicePaid tx-pay-invoice))
+
+                     tx-cancel-invoice (<! (smart-contracts/contract-send [:job job-address] :cancel-invoice [invoice-id "0x0"] {:from worker}))
+                     invoice-raw (<! (smart-contracts/contract-call [:job job-address] :get-invoice [invoice-id]))
+                     invoice (js->clj invoice-raw) ; [[[[0 0x0000000000000000000000000000000000000000] 0] 0] 0x0000000000000000000000000000000000000000 0 true false]
+                     invoice-paid? (get-in invoice [3])
+                     invoice-cancelled? (get-in invoice [4])]
+                 (is (= nil tx-cancel-invoice))
+                 (is (= invoice-paid? true))
+                 (is (= invoice-cancelled? false))))
              (done)))))
