de895682cfcf23e46ac5cfaa10f4e15d8ba08070
Madis Nõmme <me@mad.is>
2021-11-05 12:24:52 +0000

    Remove outdated test & source files

--- src/ethlance/server/contract/ethlance_jobs.cljs
+++
@@ -1,9 +0,0 @@
-(ns ethlance.server.contract.ethlance-jobs
-  (:require [district.server.smart-contracts :as contracts]
-            [ethlance.server.contract.ethlance-issuer :as ethlance-issuer]))
-
-(defn accept-candidate [address args opts]
-  (contracts/contract-send [ethlance-issuer/*ethlance-jobs-key* address] :accept-candidate args (merge {:gas 5e6} opts)))
-
-(defn invoice-job [address args opts]
-  (contracts/contract-send [ethlance-issuer/*ethlance-jobs-key* address] :invoice-job args (merge {:gas 5e6} opts)))
