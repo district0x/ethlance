85136a9785ba22b888d038b2fe8cba68a1868f6c
Madis Nõmme <me@mad.is>
2023-02-13 08:09:37 -0600

    Set page title (browser) automatically based on route

--- ui/src/ethlance/ui/component/main_layout.cljs
+++ ui/src/ethlance/ui/component/main_layout.cljs
@@ -9,7 +9,18 @@
             [ethlance.ui.component.mobile-navigation-bar
              :refer
              [c-mobile-navigation-bar]]
+            [district.ui.router.subs :as router.subs]
+            [re-frame.core :as re]
             [ethlance.ui.component.sign-in-dialog :refer [c-sign-in-dialog] :as sidi]))
+
+(defn page-title-from-route-name
+  "Example: :route.job/detail => Ethlance: Job Detail"
+  [route-name]
+  (let [app-name "Ethlance"
+        name-parts-from-route-ns (rest (clojure.string/split (namespace route-name) "."))
+        name-part-from-route-name(name route-name)
+        name-parts (flatten [app-name ":" name-parts-from-route-ns name-part-from-route-name])]
+    (clojure.string/join " " (map clojure.string/capitalize name-parts))))
 
 (defn c-main-layout
   "The main layout of each page in the ethlance ui.
@@ -24,29 +35,31 @@
   that contains the child component.
   "
   []
-  (fn [{:keys [container-opts] :as opts} & children]
-    (let [opts (dissoc opts :container-opts)]
-      [:div.main-layout
-       opts
-       [c-main-navigation-bar]
-       [c-mobile-navigation-bar]
-       [:div.main-margin
-        [c-main-navigation-menu]
-        (into [:div.main-container container-opts] children)]
-       [:div.footer
-        [:div.copyright
-         [:span "Copyright © 2019 Ethlance.com"]
-         [:div.spacer "|"]
-         [:span "All rights reserved."]]
-        [:div.social
-         [c-circle-icon-button {:name :facebook :title "District0x Facebook"
-                                :size :small :href "https://www.facebook.com/district0x/"}]
-         [c-circle-icon-button {:name :twitter :title "District0x Twitter"
-                                :size :small :href "https://twitter.com/district0x?lang=en"}]
-         [c-circle-icon-button {:name :github :title "District0x Github"
-                                :size :small :href "https://github.com/district0x"}]
-         [c-circle-icon-button {:name :slack :title "District0x Slack"
-                                :size :small :href "https://district0x-slack.herokuapp.com/"}]]]
+  (let [active-page (re/subscribe [::router.subs/active-page-name])]
+    (fn [{:keys [container-opts] :as opts} & children]
+      (set! (.-title js/document) (page-title-from-route-name @active-page))
+      (let [opts (dissoc opts :container-opts)]
+        [:div.main-layout
+         opts
+         [c-main-navigation-bar]
+         [c-mobile-navigation-bar]
+         [:div.main-margin
+          [c-main-navigation-menu]
+          (into [:div.main-container container-opts] children)]
+         [:div.footer
+          [:div.copyright
+           [:span "Copyright © 2019 Ethlance.com"]
+           [:div.spacer "|"]
+           [:span "All rights reserved."]]
+          [:div.social
+           [c-circle-icon-button {:name :facebook :title "District0x Facebook"
+                                  :size :small :href "https://www.facebook.com/district0x/"}]
+           [c-circle-icon-button {:name :twitter :title "District0x Twitter"
+                                  :size :small :href "https://twitter.com/district0x?lang=en"}]
+           [c-circle-icon-button {:name :github :title "District0x Github"
+                                  :size :small :href "https://github.com/district0x"}]
+           [c-circle-icon-button {:name :slack :title "District0x Slack"
+                                  :size :small :href "https://district0x-slack.herokuapp.com/"}]]]
 
-       [:div.modals
-        [c-sign-in-dialog]]])))
+         [:div.modals
+          [c-sign-in-dialog]]]))))
