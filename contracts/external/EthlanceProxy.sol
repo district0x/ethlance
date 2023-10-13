5407889514e4addd215d6e62408f7b532401d09c
Madis Nõmme <me@mad.is>
2023-10-13 22:49:15 +0200

    Add missing contracts

---
+++ contracts/external/EthlanceProxy.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "./DelegateProxy.sol";
+import "./ds-auth/auth.sol";
+
+/**
+ * @title Forwarder proxy contract with editable target
+ */
+contract EthlanceProxy is DelegateProxy, DSAuth {
+  address public target = 0xBEeFbeefbEefbeEFbeEfbEEfBEeFbeEfBeEfBeef; // checksumed to silence warning
+
+  /**
+   * @dev Replaces targer forwarder contract is pointing to
+   * Only authenticated user can replace target
+
+   * @param _target New target to proxy into
+   */
+  function setTarget(address _target) public auth {
+    target = _target;
+  }
+
+  // event Received(address, uint);
+  // receive() external payable {
+  //   payable(target).transfer(msg.value);
+  // }
+
+  fallback() external payable {
+    delegatedFwd(target, msg.data);
+  }
+
+}
